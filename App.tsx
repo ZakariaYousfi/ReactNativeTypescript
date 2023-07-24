1f92643253fb8f156f56b02b2a19c98bfcaf9784
Yousfi Zakaria <zakonaser@gmail.com>
2023-07-24 14:32:31 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {Text, StyleSheet, View, Image, Button, ScrollView} from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  const info = require('./info.json')
+
+  const [click,setClick] = useState('')
+
+  const clicked = (Specialty : string) => {
+    setClick(Specialty)
+  }
+
+  return (
+          <ScrollView style = {{margin:20}}>
+            <Text style = {styleAppText.titleText}>KHBEESHERS DEN</Text>
+            <Image source = {{ uri: 'http://192.168.8.1/res/icon_signal_03.png', }} style={{width: 200, height: 200}}></Image>
+            <Text style = {styleAppText.titleText}>After Picture</Text>
+            { click ? <Specialty name = {click} changeSpecialty = {clicked} info ={info["Specialty"][click]}> </Specialty> :       <><Button onPress={() => clicked('ACAD')} title = 'ACAD'></Button>
+                              <Button onPress={() => clicked('ISIL')} title = 'ISIL'></Button>
+                              <Button onPress={() => clicked('SII')} title = 'SII'></Button></>
+             }
+
+          </ScrollView>
+      )
+};
+
+const styleAppText = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
+
+type SpecialtyProps = {
+  name: string,
+  changeSpecialty : (Specialty : string) => void
+  info: any
+}
+
+const Specialty = (props: SpecialtyProps) => {
+  return <ScrollView>
+    <Text style = {styleAppText.titleText} >{props.name}</Text>
+    <Button onPress={ () => props.changeSpecialty('')} title = 'Go back to Specialty change'></Button>
+    <Course info = {props.info["Course"]}></Course>
+  </ScrollView>
+}
+
+type CourseProps = {
+  info: any
+}
+
+const Course = (props: CourseProps) => {
+  return <ScrollView>
+        <View style = {{flex: 1, flexDirection:'row'}}>   
+               <View style={{flex: 1, backgroundColor: 'red'}}><Button title = "Lectures"></Button></View>
+               <View style={{flex: 2, backgroundColor: 'darkorange'}}><Button title = "ProblemSession"></Button> </View>
+               <View style={{flex: 3, backgroundColor: 'green'}}><Button title = "PracticalSession"></Button> </View>
+          </View>   
+          <View>
+            </View> 
+  </ScrollView>
+}
